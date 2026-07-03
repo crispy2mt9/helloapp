b53e2ab00d7ae173921abc1ee5c3aeee60682118
black <black@example.com>
2026-07-03 22:07:29 +0500

    Add GreetBlack method

--- HELLO.cs
+++ HELLO.cs
@@ -18,5 +18,10 @@ namespace HelloApp
         {
             Console.WriteLine("hello white");
         }
+
+        static void GreetBlack()
+        {
+            Console.WriteLine("hello black");
+        }
     }
 }
